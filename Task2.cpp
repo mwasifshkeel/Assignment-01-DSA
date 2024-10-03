e47d1a75a8ce10b4d5ce9acb93f831ed15f2aca5
Muhammad Wasif Shakeel <mshakeel.bsds23seecs@seecs.edu.pk>
2024-10-03 22:13:40 +0500

    Implemented modules to clean user input and store the number into the linked list.

--- Task2.cpp
+++ Task2.cpp
@@ -4,24 +4,25 @@ using namespace std;
 
 class node {
 public:
-  int data;
+  string data;
   node *prev = nullptr;
   node *next = nullptr;
-}
+};
 
 class Number {
+public:
   node *first = nullptr;
   node *last = nullptr;
   int numItems;
   node *Loc_ = nullptr;
   node *PLoc_ = nullptr;
 
-  bool isEmpty() { return start == nullptr; }
+  bool isEmpty() { return first == nullptr; }
 
   void PrintList() {
     if (!isEmpty()) {
       node *temp;
-      temp = start;
+      temp = first;
       while (temp != nullptr) {
         cout << temp->data;
         temp = temp->next;
@@ -32,27 +33,27 @@ class Number {
     }
   }
 
-  void Search(int key) {
+  void Search(string key) {
     if (isEmpty()) {
       return;
     }
-    Loc_ = start;
+    Loc_ = first;
     PLoc_ = nullptr;
     while (Loc_ == nullptr && Loc_->data < key) {
       PLoc_ = Loc_;
       Loc_ = Loc_->next;
     }
 
-    if (Loc_ == nullptr && Loc_->data != value) {
+    if (Loc_ == nullptr && Loc_->data != key) {
       Loc_ = nullptr;
     }
   }
 
-  void InsertAtEnd(int value) {
+  void InsertAtEnd(string value) {
     node *newNode = new node();
     newNode->data = value;
 
-    if (start == nullptr) {
+    if (first == nullptr) {
       first = newNode;
       last = newNode;
       return;
@@ -63,16 +64,16 @@ class Number {
     last = newNode;
   }
 
-  void Delete(int value) {
+  void Delete(string value) {
     Search(value);
     if (Loc_ != nullptr) {
       if (PLoc_ == nullptr) {
         if (Loc_ == last) {
-          start = nullptr;
+          first = nullptr;
           last = nullptr;
         } else {
-          start = start->next;
-          start->prev = nullptr;
+          first = first->next;
+          first->prev = nullptr;
         }
       } else {
         if (Loc_ == last) {
@@ -101,8 +102,56 @@ class Number {
     first = nullptr;
     last = nullptr;
   }
+};
+
+bool correctString(string temp) {
+  if (temp.length() == 0) {
+    return false;
+  }
+  for (int i = 0; i < temp.length(); i++) {
+    if (!((temp.at(i) >= '0') && (temp.at(i) <= '9'))) {
+      return false;
+    }
+  }
+  return true;
+}
+
+string getNumber() {
+  string temp = "";
+  cout << "Enter your number: ";
+  bool firstTime = true;
+  while (!correctString(temp)) {
+    if (!firstTime) {
+      cout << endl
+           << "You have not entered a correct number. Try Again!" << endl;
+      cout << "Enter your number: ";
+    }
+    firstTime = false;
+    getline(cin, temp);
+  }
+  return temp;
+}
+
+void storeNumber(Number *n, string num) {
+  if (num.length() < 9) {
+    (*n).InsertAtEnd(num.substr(0, num.length()));
+  } else {
+    for (int i = 0; i < num.length() - (num.length() % 9); i += 9) {
+      (*n).InsertAtEnd(num.substr(i, 9));
+    }
+
+    (*n).InsertAtEnd(num.substr(num.length() - (num.length() % 9),
+                                num.length() - (num.length() % 9)));
+  }
 }
 
 int main() {
+  string num = getNumber();
+  Number n1;
+
+  storeNumber(&n1, num);
+
+  n1.PrintList();
+
   return 0;
 }
